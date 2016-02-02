155885178e9e5437bdb0741144de7115440ffc36
Dave Thwaites <dave@thwaites.org.uk>
2016-02-02 11:06:26 +0000

    Initial commit

---
+++ ener314.py
@@ -0,0 +1,167 @@
+# Written by Dave Thwaites
+#
+# Inspiration taken from:
+# http://stackoverflow.com/questions/8600161/executing-periodic-actions-in-python#answer-8600329
+# and NodeRED nrgpio.py code
+# and Energenie's ENER002-PI2.py example code
+
+# from threading import Timer
+from threading import Thread
+from threading import Event
+from time import sleep
+from collections import deque
+import sys
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser"
+          " privileges.  You can achieve this by using 'sudo' to run your script")
+    sys.exit(0)
+
+
+# Pins for ENER314 add-on card
+ener314_D0 = 11
+ener314_D1 = 15
+ener314_D2 = 16
+ener314_D3 = 13
+ener314_ModSel = 18
+ener314_enable = 22
+
+try:
+    # set the pins numbering mode
+    GPIO.setmode(GPIO.BOARD)
+    
+    # Select the GPIO pins used for the encoder K0-K3 data inputs
+    GPIO.setup(ener314_D0, GPIO.OUT)
+    GPIO.setup(ener314_D1, GPIO.OUT)
+    GPIO.setup(ener314_D2, GPIO.OUT)
+    GPIO.setup(ener314_D3, GPIO.OUT)
+    
+    # Select the signal to select ASK/FSK
+    GPIO.setup(ener314_ModSel, GPIO.OUT)
+    
+    # Select the signal used to enable/disable the modulator
+    GPIO.setup(ener314_enable, GPIO.OUT)
+except RuntimeError:
+    print("Error setting up GPIO pins!  This is probably because you need superuser"
+          " privileges.  You can achieve this by using 'sudo' to run your script")
+    sys.exit(0)
+
+# Disable the modulator by setting CE pin lo
+GPIO.output(ener314_enable, False)
+
+# Set the modulator to ASK for On Off Keying by setting MODSEL pin low
+GPIO.output(ener314_ModSel, False)
+
+
+print ("Enter items to be added to the queue - they will be returned (roughly)"
+       "3 seconds apart")
+print "Enter 'exit' to exit."
+
+
+def bg():
+    print "Starting background thread..."
+    while not exitflag.is_set():
+        if len(queue) > 0:
+            while len(queue) > 0 and not exitflag.is_set():
+                item = queue.popleft()
+                # print "Next item: switch", item[0], "-", item[1].title()
+                send(item[0], item[1])
+                exitflag.wait(3)
+            if len(queue) > 0:
+                print len(queue), "items left in queue!"
+                pass
+            else:
+                print "No items left!"
+                pass
+    print "Stopping background thread..."
+
+def send(switch, value):
+    if switch == "1":
+        GPIO.output(ener314_D0, 1)
+        GPIO.output(ener314_D1, 1)
+        GPIO.output(ener314_D2, 1)
+        print "Switch 1",
+    elif switch == "2":
+        GPIO.output(ener314_D0, 0)
+        GPIO.output(ener314_D1, 1)
+        GPIO.output(ener314_D2, 1)
+        print "Switch 2",
+    elif switch == "3":
+        GPIO.output(ener314_D0, 1)
+        GPIO.output(ener314_D1, 0)
+        GPIO.output(ener314_D2, 1)
+        print "Switch 3",
+    elif switch == "4":
+        GPIO.output(ener314_D0, 0)
+        GPIO.output(ener314_D1, 0)
+        GPIO.output(ener314_D2, 1)
+        print "Switch 4",
+    elif switch == "all":
+        GPIO.output(ener314_D0, 1)
+        GPIO.output(ener314_D1, 1)
+        GPIO.output(ener314_D2, 0)
+        print "All switches",
+    if value == "on":
+        GPIO.output(ener314_D3, 1)
+        print "On",
+    else:
+        GPIO.output(ener314_D3, 0)
+        print "Off",
+    # let it settle, encoder requires this
+    sleep(0.1)	
+    # Enable the modulator
+    print "- sending...",
+    GPIO.output(ener314_enable, True)
+    # keep enabled for a period
+    sleep(0.25)
+    # Disable the modulator
+    GPIO.output(ener314_enable, False)
+    print "done!"
+
+
+queue = deque()
+exitflag = Event()
+bg_thread = Thread(None, bg)
+bg_thread.start()
+
+while True:
+    try:
+        ip = raw_input().lower()
+        if 'exit' in ip:
+            sys.exit(0)
+        if ip != "":
+            data = ip.split()
+            if len(data) != 2:
+                print "Bad input - expecting two parameters!"
+                continue
+            # if not data[0].isdigit() and data[0]!="all":
+            #     print "Bad input - first parameter should be a digit or 'All'"
+            #     continue
+            if ( ( data[0].isdigit() and ( int(data[0])<1 or int(data[0])>4 ) )
+                or ( not data[0].isdigit() and data[0]!="all" ) ):
+                print ("Bad input - first parameter should be a digit between"
+                       " 1-4 or 'All'")
+                continue
+            if data[1]!="on" and data[1]!="off":
+                print "Bad input - second parameter should be 'On' or 'Off'"
+                continue
+            queue.append(data)
+            # print "len(queue) =", len(queue)
+            # print "bg_thread.is_alive() =", bg_thread.is_alive()
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        print "Exiting..."
+        exitflag.set()
+        GPIO.cleanup()
+        sys.exit(0)
+    except Exception as ex:
+        print "Unexpected Exception!"
+        #print "sys.exc_info()[0]:", sys.exc_info()[0]
+        #print "ex:", ex
+        print "ip:", ip
+        #print "Exiting..."
+        exitflag.set()
+        GPIO.cleanup()
+        # sys.exit(0)
+        raise
+    
---
+++ rpi-ener.js
@@ -0,0 +1,185 @@
+/**
+ * Copyright 2015 Dave Thwaites
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var util = require("util");
+    var spawn = require('child_process').spawn;
+    var fs = require('fs');
+
+    var gpioCommand = __dirname+'/ener314';
+
+    if (!fs.existsSync("/dev/ttyAMA0")) { // unlikely if not on a Pi
+        util.log("Info : Ignoring Raspberry Pi specific node (rpi-ener314).");
+        throw "Info : Ignoring Raspberry Pi specific node (rpi-ener314).";
+    }
+
+    if (!fs.existsSync("/usr/share/doc/python-rpi.gpio")) {
+        util.log("[rpi-ener314] Warning : Can't find RPi.GPIO python library.");
+        throw "Warning : Can't find RPi.GPIO python library.";
+    }
+
+    if ( !(1 & parseInt ((fs.statSync(gpioCommand).mode & parseInt ("777", 8)).toString (8)[0]) )) {
+        util.log("[rpi-ener314] Error : "+gpioCommand+" needs to be executable.");
+        throw "Error : ever314.py needs to be executable.";
+    }
+
+
+/*
+    function PibrellaIn(n) {
+        RED.nodes.createNode(this,n);
+        this.buttonState = -1;
+        this.pin = pintable[n.pin];
+        this.read = n.read || false;
+        if (this.read) { this.buttonState = -2; }
+        var node = this;
+        if (!pinsInUse.hasOwnProperty(this.pin)) {
+            pinsInUse[this.pin] = "tri";
+        }
+        else {
+            if ((pinsInUse[this.pin] !== "tri")||(pinsInUse[this.pin] === "pwm")) {
+                node.error("GPIO pin "+this.pin+" already set as "+pinTypes[pinsInUse[this.pin]]);
+            }
+        }
+
+        if (node.pin !== undefined) {
+            node.child = spawn(gpioCommand, ["in",node.pin]);
+            node.running = true;
+            node.status({fill:"green",shape:"dot",text:"OK"});
+
+            node.child.stdout.on('data', function (data) {
+                data = data.toString().trim();
+                if (data.length > 0) {
+                    if (node.buttonState !== -1) {
+                        node.send({ topic:"pibrella/"+tablepin[node.pin], payload:Number(data) });
+                    }
+                    node.buttonState = data;
+                    node.status({fill:"green",shape:"dot",text:data});
+                    if (RED.settings.verbose) { node.log("out: "+data+" :"); }
+                }
+            });
+
+            node.child.stderr.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("err: "+data+" :"); }
+            });
+
+            node.child.on('close', function (code) {
+                if (RED.settings.verbose) { node.log("ret: "+code+" :"); }
+                node.child = null;
+                node.running = false;
+                node.status({fill:"red",shape:"circle",text:""});
+            });
+
+            node.child.on('error', function (err) {
+                if (err.errno === "ENOENT") { node.warn('Command not found'); }
+                else if (err.errno === "EACCES") { node.warn('Command not executable'); }
+                else { node.log('error: ' + err); }
+            });
+
+        }
+        else {
+            node.error("Invalid GPIO pin: "+node.pin);
+        }
+
+        node.on("close", function() {
+            if (node.child != null) {
+                node.child.stdin.write(" close "+node.pin);
+                node.child.kill('SIGKILL');
+            }
+            node.status({fill:"red",shape:"circle",text:""});
+            delete pinsInUse[node.pin];
+            if (RED.settings.verbose) { node.log("end"); }
+        });
+    }
+    RED.nodes.registerType("rpi-pibrella in",PibrellaIn);
+*/
+
+
+    function ener314(n) {
+        RED.nodes.createNode(this,n);
+        this.socket = n.socket;
+        this.set = n.set || false;
+        this.state = n.state || 0;
+        var node = this;
+
+        function inputlistener(msg) {
+            if (msg.payload === "true") { msg.payload = true; }
+            if (msg.payload === "false") { msg.payload = false; }
+            var out = Number(msg.payload);
+            if ((out >= 0) && (out <= 1)) {
+                if (RED.settings.verbose) { node.log("inp: "+msg.payload); }
+                if (node.child !== null) { node.child.stdin.write(this.socket+" "+out+"\n"); }
+                else { node.warn("Command not running"); }
+                node.status({fill:"green",shape:"circle",text:msg.payload});
+            }
+            else { node.warn("Invalid input: "+out); }
+        }
+
+        if (node.socket !== undefined) {
+            node.child = spawn(gpioCommand, []);
+            if (node.set) {
+                node.child.stdin.write(node.socket+" "+node.state+"\n");
+                node.status({fill:"green",shape:"circle",text:node.state});
+            } else {
+                node.status({fill:"green",shape:"dot",text:"OK"});
+            }
+            node.running = true;
+
+            node.on("input", inputlistener);
+
+            node.child.stdout.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("out: "+data+" :"); }
+            });
+
+            node.child.stderr.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("err: "+data+" :"); }
+            });
+
+            node.child.on('close', function (code) {
+                if (RED.settings.verbose) { node.log("ret: "+code+" :"); }
+                node.child = null;
+                node.running = false;
+                node.status({fill:"red",shape:"circle",text:""});
+            });
+
+            node.child.on('error', function (err) {
+                if (err.errno === "ENOENT") { node.warn('Command not found'); }
+                else if (err.errno === "EACCES") { node.warn('Command not executable'); }
+                else { node.log('error: ' + err); }
+            });
+
+        } else {
+            node.error("Invalid ENER314 socket: "+node.socket);
+        }
+
+        node.on("close", function() {
+            if (node.child != null) {
+                node.child.stdin.write("exit");
+                node.child.kill('SIGKILL');
+            }
+            node.status({fill:"red",shape:"circle",text:""});
+            if (RED.settings.verbose) { node.log("end"); }
+        });
+
+    }
+    RED.nodes.registerType("rpi-ener314",ener314);
+
+/*
+    RED.httpAdmin.get('/rpi-pibpins/:id',RED.auth.needsPermission('rpi-pibrella.read'),function(req,res) {
+        res.json(pinsInUse);
+    });
+*/
+}
