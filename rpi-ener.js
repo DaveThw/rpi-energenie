910b97772a3dd610141eb1cff8a514102e1f9b9c
Dave Thwaites <github@dave.thwaites.org.uk>
2016-02-04 20:11:47 +0000

    Update year of copyright to 2016

--- rpi-ener.js
+++ rpi-ener.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Dave Thwaites
+ * Copyright 2016 Dave Thwaites
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
